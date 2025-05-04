b6d41adff6c444495398a36ea67a36c78f32c5b5
YasasiKariyawasam <158291905+YasasiKariyawasam@users.noreply.github.com>
2025-05-04 21:28:02 +0530

    Fix deployment dependencies

--- frontend/jest.setup.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Fix for TextEncoder/TextDecoder
-import { TextEncoder, TextDecoder } from 'util';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
